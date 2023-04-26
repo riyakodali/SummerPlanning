fc770f6739a94e23ec2e64babcbef623a7abc615
Riya Kodali <riyakodali@gmail.com>
2023-04-26 14:00:21 -0400

    Initial Commit

---
+++ SummerPlanning/SummerPlanningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SummerPlanningApp.swift
+//  SummerPlanning
+//
+//  Created by Riya Kodali on 4/26/23.
+//
+
+import SwiftUI
+
+@main
+struct SummerPlanningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
